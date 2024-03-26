16c821575ebd58bfe6573afb1baa1de43ff766c8
Minerva Curtis <mintentha@gmail.com>
2024-03-26 00:58:09 -0400

    Added V3 and V4 classes

---
+++ 4DRayTracer/V3.cpp
@@ -0,0 +1,184 @@
+#include "V3.h"
+
+V3::V3() {
+	V3(0.0f);
+}
+
+V3::V3(float x, float y, float z) {
+	xyz[0] = x;
+	xyz[1] = y;
+	xyz[2] = z;
+}
+
+V3::V3(const V3& v) {
+	xyz[0] = v.x;
+	xyz[1] = v.y;
+	xyz[2] = v.z;
+}
+
+V3::V3(unsigned int col) {
+	this->SetColor(col);
+}
+
+float& V3::operator[](int i) {
+
+	return xyz[i];
+
+}
+
+V3& V3::operator=(V3 v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	return *this;
+}
+
+V3& V3::operator+=(V3 v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
+}
+
+V3& V3::operator-=(V3 v) {
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	return *this;
+}
+
+V3& V3::operator*=(float scf) {
+	x *= scf;
+	y *= scf;
+	z *= scf;
+	return *this;
+}
+
+V3& V3::operator/=(float scf) {
+	x /= scf;
+	y /= scf;
+	z /= scf;
+	return *this;
+}
+
+V3& V3::operator^=(V3 v) {
+	return *this = *this ^ v;
+}
+
+V3& V3::operator%=(V3 v) {
+	x *= v.x;
+	y *= v.y;
+	z *= v.z;
+	return *this;
+}
+
+bool V3::operator==(V3 v) {
+	return (x == v.x && y == v.y && z == v.z);
+}
+
+bool V3::operator>=(V3 v) {
+	return (x >= v.x && y >= v.y && z >= v.z);
+}
+
+bool V3::operator<=(V3 v) {
+	return (x <= v.x && y <= v.y && z <= v.z);
+}
+
+bool V3::operator>(V3 v) {
+	return (x > v.x && y > v.y && z > v.z);
+}
+
+bool V3::operator<(V3 v) {
+	return (x < v.x && y < v.y && z < v.z);
+}
+
+V3 operator+(V3 v1, V3 v2) {
+	return v1 += v2;
+}
+
+V3 operator-(V3 v1, V3 v2) {
+	return v1 -= v2;
+}
+
+V3 operator*(V3 v1, float scf) {
+	return v1 *= scf;
+}
+
+V3 operator/(V3 v1, float scf) {
+	return v1 /= scf;
+}
+
+V3 V3::operator^(V3 v) {
+	return V3(y * v.z - z * v.y,
+		z * v.x - x * v.z,
+		x * v.y - y * v.x);
+}
+
+V3 operator%(V3 v1, V3 v2) {
+	return v1 %= v2;
+}
+
+float V3::operator*(V3 v) {
+	return x * v.x + y * v.y + z * v.z;
+}
+
+V3 V3::operator-() {
+	return V3(-x, -y, -z);
+}
+
+V3 operator*(float scf, V3 v) {
+	return v * scf;
+}
+
+std::ostream& operator<<(std::ostream& ostr, V3& v) {
+
+	return ostr << v[0] << " " << v[1] << " " << v[2];
+
+}
+
+std::istream& operator>>(std::istream& istr, V3& v) {
+	return istr >> v[0] >> v[1] >> v[2];
+}
+
+V3 &V3::normalize() {
+	float l = length();
+	if (l == 0) {
+		return *this;
+	}
+	return *this /= l;
+}
+
+V3 V3::normalized() {
+	return V3(*this).normalize();
+}
+
+
+float V3::l2squared() {
+	return (x * x + y * y + z * z);
+}
+
+float V3::length() {
+	return sqrtf(l2squared());
+}
+
+unsigned int V3::GetColor() {
+	unsigned int ret;
+	unsigned char* rgba = (unsigned char*) &ret;
+	for (int ci = 0; ci < 3; ci++) {
+		if (xyz[ci] < 0.0f)
+			rgba[ci] = 0;
+		else if (xyz[ci] >= 1.0f)
+			rgba[ci] = 255;
+		else
+			rgba[ci] = (unsigned char) (xyz[ci] * 255.0f);
+	}
+	rgba[3] = 0xFF;
+	return ret;
+}
+
+void V3::SetColor(unsigned int color) {
+	unsigned char* rgba = (unsigned char*)&color;
+	for (int ci = 0; ci < 3; ci++) {
+		xyz[ci] = (float) rgba[ci] / 255.0f;
+	}
+}
---
+++ 4DRayTracer/V4.cpp
@@ -0,0 +1,156 @@
+#include "V4.h"
+
+V4::V4() {
+	V4(0.0f);
+}
+
+V4::V4(float x, float y, float z, float w) {
+	xyzw[0] = x;
+	xyzw[1] = y;
+	xyzw[2] = z;
+	xyzw[3] = w;
+}
+
+V4::V4(const V4& v) {
+	xyzw[0] = v.x;
+	xyzw[1] = v.y;
+	xyzw[2] = v.z;
+	xyzw[3] = v.w;
+}
+
+float& V4::operator[](int i) {
+
+	return xyzw[i];
+
+}
+
+V4& V4::operator=(V4 v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	w = v.w;
+	return *this;
+}
+
+V4& V4::operator+=(V4 v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	w += v.w;
+	return *this;
+}
+
+V4& V4::operator-=(V4 v) {
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	w -= v.w;
+	return *this;
+}
+
+V4& V4::operator*=(float scf) {
+	x *= scf;
+	y *= scf;
+	z *= scf;
+	w *= scf;
+	return *this;
+}
+
+V4& V4::operator/=(float scf) {
+	x /= scf;
+	y /= scf;
+	z /= scf;
+	w /= scf;
+	return *this;
+}
+
+V4& V4::operator%=(V4 v) {
+	x *= v.x;
+	y *= v.y;
+	z *= v.z;
+	w *= v.w;
+	return *this;
+}
+
+bool V4::operator==(V4 v) {
+	return (x == v.x && y == v.y && z == v.z && w == v.w);
+}
+
+bool V4::operator>=(V4 v) {
+	return (x >= v.x && y >= v.y && z >= v.z && w >= v.w);
+}
+
+bool V4::operator<=(V4 v) {
+	return (x <= v.x && y <= v.y && z <= v.z && w <= v.w);
+}
+
+bool V4::operator>(V4 v) {
+	return (x > v.x && y > v.y && z > v.z && w > v.w);
+}
+
+bool V4::operator<(V4 v) {
+	return (x < v.x && y < v.y && z < v.z && w < v.w);
+}
+
+V4 operator+(V4 v1, V4 v2) {
+	return v1 += v2;
+}
+
+V4 operator-(V4 v1, V4 v2) {
+	return v1 -= v2;
+}
+
+V4 operator*(V4 v1, float scf) {
+	return v1 *= scf;
+}
+
+V4 operator/(V4 v1, float scf) {
+	return v1 /= scf;
+}
+
+V4 operator%(V4 v1, V4 v2) {
+	return v1 %= v2;
+}
+
+float V4::operator*(V4 v) {
+	return x * v.x + y * v.y + z * v.z;
+}
+
+V4 V4::operator-() {
+	return V4(-x, -y, -z, -w);
+}
+
+V4 operator*(float scf, V4 v) {
+	return v * scf;
+}
+
+std::ostream& operator<<(std::ostream& ostr, V4& v) {
+
+	return ostr << v[0] << " " << v[1] << " " << v[2] << " " << v[3];
+
+}
+
+std::istream& operator>>(std::istream& istr, V4& v) {
+	return istr >> v[0] >> v[1] >> v[2] >> v[3];
+}
+
+V4& V4::normalize() {
+	float l = length();
+	if (l == 0) {
+		return *this;
+	}
+	return *this /= l;
+}
+
+V4 V4::normalized() {
+	return V4(*this).normalize();
+}
+
+
+float V4::l2squared() {
+	return (x * x + y * y + z * z + w * w);
+}
+
+float V4::length() {
+	return sqrtf(l2squared());
+}
