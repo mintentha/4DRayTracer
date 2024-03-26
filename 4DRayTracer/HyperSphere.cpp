72696ddb50a6bbdad3795b0ff23538cd0ba20f94
Minerva Curtis <mintentha@gmail.com>
2024-03-26 12:42:47 -0400

    Added intersection algorithm for HyperSpheres

--- 4DRayTracer/HyperSphere.cpp
+++ 4DRayTracer/HyperSphere.cpp
@@ -8,7 +8,28 @@ HyperSphere::HyperSphere(V4 C, float r, Material material) {
 }
 
 V4 HyperSphere::getIntersection(V4 o, V4 dir, float *time) {
-	// TODO:
+	dir.normalize();
+	// float a = dir.l2squared();
+	// a = 1
+	float b = 2 * dir * (o - C);
+	float c = (o - C).l2squared() - r * r;
+	float disc = b * b - 4 * c;
+	if (disc < 0) {
+		*time = -1.0f;
+		return V4(0.0f);
+	}
+	float t1 = -b - sqrtf(disc);
+	float t2 = -b + sqrtf(disc);
+	if (t1 > 0) {
+		*time = t1;
+		return o + dir * t1;
+	}
+	if (t2 > 0) {
+		*time = t2;
+		return o + dir * t2;
+	}
+	*time = -1.0f;
+	return V4(0.0f);
 }
 
 V4 HyperSphere::getNormal(V4 p) {
