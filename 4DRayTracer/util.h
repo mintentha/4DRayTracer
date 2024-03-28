6f2d34ffde69818f19dff3b0126ee4a3460578ec
Minerva Curtis <mintentha@gmail.com>
2024-03-28 16:52:12 -0400

    Actually removed util files

--- 4DRayTracer/util.h
+++
@@ -1,6 +0,0 @@
-#include "V3.h"
-#include "V4.h"
-#include <math.h>
-float distToHyperplane(V4 o, V4 d, V4 n, V4 a);
-V4 bary4D(V4 p, V4 t1, V4 t2, V4 t3, V4 t4);
-float det2(float a1, float a2, float b1, float b2);
